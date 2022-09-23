ff7cde1c3819fe274909aa048098b472e3afdfce
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-09-22 22:45:43 -0300

    Adding to GitHub

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const Navigation = () => {
+    return(
+        <nav style={{position: 'absolute', right: 0}}>
+            <p>Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
